9c73210e70d1de95859f324ff12023ee58fac67e
Julien264 <deajulien2005@gmail.com>
2026-05-14 18:26:14 -0400

    javascript connecté (et presque fonctionel)

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const employes = document.querySelectorAll('.employe');
+const modal = document.querySelector('.modal');
+
+for (let i = 0; i < employes.length; i++) {
+    const button = employes[i];
+    
+    button.addEventListener('click', openModal);
+}
+
+function openModal(){
+    
+    const path = this.src;
+    const image = document.createElement('img');
+    image.src = path;
+    image.classList.add('zoom');
+    
+    modal.appendChild(image);
+    modal.classList.add('visible');
+
+    // Fermeture de la modale
+    image.addEventListener('click', closeModal);
+}
+
+function closeModal(event){
+    modal.classList.remove("visible");
+    modal.removeChild(event.currentTarget);
+}
